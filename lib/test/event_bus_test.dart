3261e1316515694a9091e9878f268464643c0547
yongfeng <2499522170@qq.com>
2024-09-22 19:27:48 +0800

    1. 添加eventbus以及使用测试

--- lib/test/event_bus_test.dart
+++ lib/test/event_bus_test.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:event_bus/event_bus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_widgets/base/base_scaffold.dart';
 
@@ -11,7 +14,21 @@ class EventBusTest extends StatefulWidget {
   State<EventBusTest> createState() => _EventBusTestState();
 }
 
-class _EventBusTestState extends State<EventBusTest> {
+class _EventBusTestState extends State<EventBusTest> with EventBusMixin {
+  int count = 0;
+  String name = "";
+
+  @override
+  void initState() {
+    super.initState();
+    onEvent((event) => debugPrint('event fired:  ${event.runtimeType}'));
+
+    onEvent<UserLoggedInEvent>((event) {
+      name = event.name;
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return BaseScaffold(
@@ -22,9 +39,47 @@ class _EventBusTestState extends State<EventBusTest> {
         children: [
           const SizedBox(width: double.infinity),
           const Text('eventbus使用测试'),
-          TextButton(onPressed: () {}, child: const Text('发送事件')),
+          TextButton(
+              onPressed: () {
+                eventBus.fire(UserLoggedInEvent('zhangsan${count++}'));
+              },
+              child: const Text('发送事件')),
+          Text('收到事件:$name'),
         ],
       ),
     );
   }
+}
+
+EventBus eventBus = EventBus();
+
+mixin EventBusMixin<S extends StatefulWidget> on State<S> {
+  final List<StreamSubscription<dynamic>> _listeners = <StreamSubscription<dynamic>>[];
+
+  void onEvent<T>(void Function(T event)? onData,
+      {Function? onError, void Function()? onDone, bool? cancelOnError}) {
+    final ss = eventBus
+        .on<T>()
+        .listen(onData, onError: onError, onDone: onDone, cancelOnError: cancelOnError);
+    _listeners.add(ss);
+  }
+
+  void sendEvent(event) {
+    eventBus.fire(event);
+  }
+
+  @override
+  void dispose() {
+    _listeners
+      ..forEach((StreamSubscription<dynamic> it) => it.cancel())
+      ..clear();
+    debugPrint("_listeners=${_listeners.length}");
+    super.dispose();
+  }
+}
+
+class UserLoggedInEvent {
+  String name;
+
+  UserLoggedInEvent(this.name);
 }
